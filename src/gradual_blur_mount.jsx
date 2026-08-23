f278f507bfdb5abd7e166c7d7dca4fd9e26fe04a
gerardetarigan79 <gerard.e.tarigan@gmail.com>
2026-08-24 01:24:14 +0700

    feat: wire GradualBlur into site

---
+++ src/gradual_blur_mount.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import GradualBlur from './GradualBlur';
+
+const mount = document.createElement('div');
+mount.id = 'gradual-blur-mount';
+Object.assign(mount.style, {
+  position: 'fixed',
+  inset: '0',
+  pointerEvents: 'none',
+  zIndex: '1000'
+});
+document.body.appendChild(mount);
+
+createRoot(mount).render(
+  <GradualBlur
+    target="parent"
+    position="bottom"
+    height="6rem"
+    strength={2}
+    divCount={5}
+    curve="bezier"
+    exponential={true}
+    opacity={1}
+  />
+);
