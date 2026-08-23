7a30837a08b00dd836fdbbb71923201770baede6
gerardetarigan79 <gerard.e.tarigan@gmail.com>
2026-08-24 01:41:36 +0700

    feat: boost global click sound to 5x

--- src/global_click_sound.js
+++ src/global_click_sound.js
@@ -1,12 +1,33 @@
 const clickAudio = new Audio('/audio/soundclick.mp3');
 clickAudio.preload = 'auto';
-clickAudio.volume = 1;
+
+let audioContext = null;
+let gainNode = null;
+
+function getAudioGraph() {
+  if (!audioContext) {
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    gainNode = audioContext.createGain();
+    gainNode.gain.value = 5;
+    gainNode.connect(audioContext.destination);
+  }
+  return { audioContext, gainNode };
+}
 
 function playClickSound() {
-  const sound = clickAudio.cloneNode(true);
-  sound.volume = 1;
-  sound.currentTime = 0;
-  sound.play().catch(() => {});
+  try {
+    const { audioContext, gainNode } = getAudioGraph();
+    if (audioContext.state === 'suspended') audioContext.resume().catch(() => {});
+
+    const sound = clickAudio.cloneNode(true);
+    sound.volume = 1;
+    const source = audioContext.createMediaElementSource(sound);
+    source.connect(gainNode);
+    sound.currentTime = 0;
+    sound.play().catch(() => {});
+  } catch (_) {
+    // Gracefully ignore browsers that block or don't support Web Audio.
+  }
 }
 
 window.addEventListener('click', (event) => {
