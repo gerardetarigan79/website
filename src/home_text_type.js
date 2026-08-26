b7fa0c635a51d5809c936418a5bfcbd146071aaa
gerardetarigan79 <gerard.e.tarigan@gmail.com>
2026-08-26 10:24:43 +0700

    feat: wire TextType into home page Draven heading

---
+++ src/home_text_type.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import TextType from './TextType.jsx';
+
+const mount = () => {
+  const heading = document.querySelector('#home h1');
+  if (!heading || heading.dataset.textTypeMounted === '1') return;
+  heading.dataset.textTypeMounted = '1';
+  createRoot(heading).render(
+    <TextType
+      as="span"
+      text={["Draven"]}
+      typingSpeed={75}
+      pauseDuration={1500}
+      showCursor={true}
+      cursorCharacter="|"
+      variableSpeed={{ min: 55, max: 95 }}
+      loop={false}
+    />
+  );
+};
+
+const observer = new MutationObserver(() => mount());
+observer.observe(document.getElementById('root') || document.body, { childList: true, subtree: true });
+mount();
